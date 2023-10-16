604816ce64d4edf6aa075b831dd139b69f855047
Sergio Fabian <riquelme.sergiofabian@gmail.com>
2023-10-16 00:50:15 -0300

    Nuevos ingresos

---
+++ curso-python/08-if.py
@@ -0,0 +1,10 @@
+puntaje = 49
+
+if puntaje >= 95:
+    print("Aprobado con honores")
+elif puntaje >= 50:
+    print("Alumno aprobado")
+else:
+    print("Alumno reprobado")
+
+print("Fuera del if")
